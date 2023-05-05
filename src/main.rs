8111173e662947349d35e7b3ea8e74fb30517211
rifkan <mohamedrifkan00127@gmail.com>
2023-05-05 15:27:46 +0530

    added comment and fixed indentation mistake

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,32 @@ use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
+    // guessing game
     println!("Guess the number");
     let secret_number = rand::thread_rng().gen_range(1..=100);
     
     loop {
-    println!("Please input your guessed number: ");
+        println!("Please input your guessed number: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line.");
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
 
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed the number {guess}");
+        println!("You guessed the number {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small."),
-        Ordering::Equal => {
-            println!("You win.");
-            break;
-        },
-        Ordering::Greater => println!("Too big.")
-    }   
-}
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small."),
+            Ordering::Equal => {
+                println!("You win.");
+                break;
+            },
+            Ordering::Greater => println!("Too big.")
+        }   
+    }
 }
