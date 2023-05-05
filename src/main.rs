34dc14b6c07b00aa6a7cf8295c09daab2a2ae3e8
rifkan <mohamedrifkan00127@gmail.com>
2023-05-05 10:42:28 +0530

    fixed the bug |genrating new random number in every loop|

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,29 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
     println!("The secret number {secret_number} ");
 
     println!("Please input your guessed number: ");
 
     let mut guess = String::new();
 
-    
+
     io::stdin().read_line(&mut guess).expect("Failed to read line.");
-    
+
     let guess: u32 = guess.trim().parse().expect("Please type a number");
 
     println!("You guessed the number {guess}");
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small."),
-        Ordering::Equal => println!("You win."),
+        Ordering::Equal => {
+            println!("You win.");
+            break;
+        },
         Ordering::Greater => println!("Too big.")
-    }
+    }   
+}
 }
