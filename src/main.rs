fa253571095ab737c284daf4283057b3131420b8
rifkan <mohamedrifkan00127@gmail.com>
2023-05-05 10:49:58 +0530

    removed secret number desplaying in screen so game can be fun

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     
     loop {
-    println!("The secret number {secret_number} ");
-
     println!("Please input your guessed number: ");
 
     let mut guess = String::new();
