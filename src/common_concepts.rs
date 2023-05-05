9f192190e18400903e04e305603eea6907e25b20
rifkan <mohamedrifkan00127@gmail.com>
2023-05-05 15:46:16 +0530

    variables -- let keyword --

--- src/common_concepts.rs
+++ src/common_concepts.rs
@@ -1,3 +1,23 @@
 pub fn common_concepts() {
-    println!("Hello")
+
+    /* 
+    
+    you cannot re-assign a value if you declare a variable in rust bu default
+
+    let x = 5;
+    println!("The value of {x}")
+    x = 6;
+    println!("The value of {x}")
+
+    ------- cannot mutate immutable variable `x` -------
+    
+    
+    */
+
+    // if  you add * mut * keyword before variable it will become mutable that me you can reassign latter
+    let mut x = 5;
+    println!("The value of {x}");
+    x = 6;
+    println!("The value of {x}");
+
 }
